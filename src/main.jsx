04017652c41056c86b9e97f7758360cb9b275eaf
joe060831-commits <joe060831@gmail.com>
2026-06-12 05:59:18 -0700

    Add package.json, vite.config.js, and src/main.jsx for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EarthAndYou from './earth_and_you.jsx'
+import EarthAndYou from '../earth_and_you.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
