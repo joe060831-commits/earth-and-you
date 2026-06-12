64de566f07ef3ff2b105abdcd997770260976dfb
joe060831-commits <joe060831@gmail.com>
2026-06-12 05:51:29 -0700

    Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EarthAndYou from './earth_and_you.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EarthAndYou />
+  </React.StrictMode>,
+)
